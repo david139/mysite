106ffd4734ab8547f50554ec4891b0bf45cb4a31
David Dudas <whatanaab@gmail.com>
2017-08-03 18:50:34 +0200

    change data type

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from mysite.recipes import views as recipe_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', recipe_views.recipes, name='list_recipes'),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]

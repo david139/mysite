35ea29a0a1675a93554625d4d67cb60b2b6ee0c8
David Dudas <whatanaab@gmail.com>
2017-08-03 16:28:19 +0200

    adding model to admin

--- mysite/recipes/admin.py
+++ mysite/recipes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from mysite.recipes.models import Recipe
 
-# Register your models here.
+
+admin.site.register(Recipe)
--- mysite/recipes/migrations/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from mysite.recipes.models import Recipe
-
-
-admin.site.register(Recipe)
